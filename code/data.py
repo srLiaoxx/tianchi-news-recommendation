82c91582696de7bc42b0c15f6aa2713ecadbbc1f
srLiaoxx <168715101+srLiaoxx@users.noreply.github.com>
2024-12-21 16:32:50 +0800

    Update data.py

--- code/data.py
+++ code/data.py
@@ -109,7 +109,7 @@ def data_online(df_train_click, df_test_click):
 
 if __name__ == '__main__':
     df_train_click = pd.read_csv('../tcdata/train_click_log.csv')
-    df_test_click = pd.read_csv('../tcdata/testB_click_log_Test_B.csv')
+    df_test_click = pd.read_csv('../tcdata/testA_click_log.csv')
 
     log.debug(
         f'df_train_click shape: {df_train_click.shape}, df_test_click shape: {df_test_click.shape}'
